9ba93ef63ab0cb4720566949bec1ffc0d0971f61
mukul <mukul.chauhan.5555@gmail.com>
2025-10-05 16:56:53 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+
+print("Hello World this is Mukul")
+print("*" * 10)
+
+x = 1
+y = 2
+
+unit_price = 3
+
+course = "   Python Programming"
+
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.rstrip())
+
+
+first = "MUKUL"
+last = "CHAUHAN"
+
+full = first + " " + last
+print(full)
+
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+
+age = 18
+message = "Eligible" if age >= 18 else "Not Eligible"
+print(message)
